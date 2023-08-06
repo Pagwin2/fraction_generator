de72a7a08a91aad5ce96bc21b2d87f51f4a8cfd9
Pagwin <git@pagwin.xyz>
2023-08-06 02:01:55 -0400

    now working faster

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate anyhow;
 extern crate indicatif;
+extern crate tokio;
+extern crate futures;
 
-use std::fs::OpenOptions;
+use tokio::fs::OpenOptions;
+use tokio::io::AsyncWriteExt;
+
 use std::ops::Add;
-use std::io::Write;
+use std::future::Future;
+use std::pin::Pin;
 
 #[derive(Clone, Copy)]
 struct Frac(u16,u16);
@@ -14,44 +19,51 @@ impl Add for Frac{
         Frac(self.0+rhs.0,self.1+rhs.1)
     }
 }
-fn main()->anyhow::Result<()>{
-    let mut fracs = Vec::<Frac>::new();
-    fracs.reserve_exact(2_usize.pow(16)+1);
 
-    // gotta seed the list with 2 starting values due to the algorithm being used(these are the
-    // upper and lower bounds)
-    fracs.push(Frac(0,1));
-    fracs.push(Frac(1,0));
+#[tokio::main]
+async fn main()->anyhow::Result<()>{
+    const RECURSIONS:u64 = 19;
 
-    #[allow(non_upper_case_globals)]
-    const iterations:usize = 19;
-
-    for i in 0..iterations{
-        //if i%10 ==0 {
-        //    eprintln!("{}\t{}",i,fracs.len());
-        //}
-        eprintln!("{}",i);
-        step(&mut fracs);
-    }
+    let prog = indicatif::ProgressBar::new(2_u64.pow(RECURSIONS as u32)-1);
+    let fracs = recurse(Frac(0,1),Frac(1,0),RECURSIONS,Some(prog)).await;
 
     let mut opts = OpenOptions::new();
     let mut file = opts.create(true)
         .write(true)
         .append(true)
-        .open("./fracts")?;
-    // might be worth adding some logic to add line numbers and remove 1/0
+        .open("./fracts").await?;
+
+    // might be worth adding some logic to add line numbers
     for frac in fracs{
-        let _ = file.write_fmt(format_args!("{}/{}\n",frac.0,frac.1));
+        let _ = file.write_all(format!("{}/{}\n",frac.0,frac.1).as_bytes()).await;
     }
+
     Ok(())
 }
-fn step(list:&mut Vec<Frac>){
-    let mut i = 0;
-    let bar = indicatif::ProgressBar::new(list.len() as u64);
-    while i < list.len()-1{
-        bar.inc(1);
-        list.insert(i+1,list[i]+list[i+1]);
-        i+=2;
-    }
-    bar.finish_and_clear();
+
+/// Returns all the fractions generated but not f1 and f2
+/// pinned box is necessary to allow recursion
+fn recurse(f1:Frac,f2:Frac, remaining:u64, progress:Option<indicatif::ProgressBar>)->Pin<Box<dyn Future<Output = Vec<Frac>> + Send + 'static>>{
+    // the things I do for recursion
+    Box::pin(async move{
+        if remaining == 0 {
+            return Vec::new()
+        }
+        let middle = f1+f2;
+        if let Some(bar) = progress.clone(){
+            tokio::task::spawn_blocking(move ||bar.inc(1));
+        }
+        let left_task = tokio::task::spawn(recurse(f1,middle,remaining-1,progress.clone()));
+        let right_task = tokio::task::spawn(recurse(middle,f2,remaining-1,progress));
+
+        let left = left_task.await.expect("left future failure");
+        let mut right = right_task.await.expect("right future failure");
+        let mut ret = left;
+        ret.reserve(ret.len()+right.len()+1);
+        ret.push(middle);
+        ret.append(&mut right);
+
+        return ret
+    })
 }
+
