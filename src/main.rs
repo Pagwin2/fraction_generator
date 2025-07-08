9dfd00d091ebce811692cdae6a42866e4d0e3f8a
Pagwin <pgp2024@pagwin.xyz>
2025-07-08 14:11:20 -0400

    single threaded version of improved thingy

--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,114 @@
 extern crate anyhow;
 extern crate indicatif;
 
+use std::cmp::PartialOrd;
+use std::collections::VecDeque;
+use std::fmt::{Debug, Display};
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::mem::MaybeUninit;
 use std::ops::Add;
-use std::cmp::PartialOrd;
-use std::fmt::{Display,Debug};
-use std::collections::VecDeque;
-use std::sync::{Arc, RwLock};
 
-fn main()->anyhow::Result<()>{
-    const RECURSIONS:u64 = 26;
-    const ELEM_COUNT:usize = 2_usize.pow(RECURSIONS as u32)-1;
-    let prog = indicatif::ProgressBar::new((2*ELEM_COUNT) as u64);
-    //let fracs = recurse(Frac(0,1),Frac(1,0),RECURSIONS,Some(prog));
-    
-    // need to stick the buffer onto the heap so we can send it between threads
-    // could init as zeroed or uninit for speed gain maybe but eh rust compiler can probably figure
-    // it out
-    let buffer: &'static mut [Frac] = Box::leak(Box::<[Frac;ELEM_COUNT]>::new([Frac(0,0);ELEM_COUNT]));
-
-    //we're never going to touch buffer and fracs at the same time but I don't want to bother
-    //proving that to the rust compiler with lifetimes
-    let fracs = unsafe{(buffer as *mut [Frac]).clone().as_ref().unwrap()};
-
-    let mut queue = VecDeque::<QueueTask>::with_capacity(ELEM_COUNT);
-    queue.push_back(QueueTask{buffer,frac1:Frac(0,1),frac2:Frac(1,0)});
-    buf_gen_fracs::<1>(queue, Some(prog));
+fn main() -> anyhow::Result<()> {
+    const THREAD_RECURSIONS = 2;
+    const RECURSIONS: u32 = 29;
+    const ELEM_COUNT: u64 = 2_u64.pow(RECURSIONS) + 1;
+    let prog = indicatif::ProgressBar::new(ELEM_COUNT);
 
     let mut opts = OpenOptions::new();
-    let mut file = opts.create(true)
+    let mut file = opts
+        .create(true)
         .write(true)
-        .append(true)
+        .append(false)
         .open("./fracts")?;
-
+    let fracs = recurse(Frac(0, 1), Frac(1, 0), RECURSIONS, Some(prog));
     // might be worth adding some logic to add line numbers
-    for frac in fracs{
-        let _ = file.write_all(format!("{}/{}\n",frac.0,frac.1).as_bytes());
+    for frac in fracs {
+        let _ = file.write_all(format!("{}/{}\n", frac.0, frac.1).as_bytes());
     }
 
     Ok(())
 }
 
-struct QueueTask {
-    frac1: Frac,
-    frac2: Frac,
-    buffer: &'static mut [Frac],
+struct Task {
+    //
+    f1: (Frac, usize),
+    f2: (Frac, usize),
+    remaining_depth: u32,
 }
-type QueueType<T> = Arc<RwLock<VecDeque<T>>>;
-//queue is taken as an argument to allow for optimizations around the buffer being a fixed number
-//of elements
-fn buf_gen_fracs<const THREAD_COUNT:usize>(queue:VecDeque<QueueTask>, bar: Option<indicatif::ProgressBar>){
-    let shared_queue:QueueType<QueueTask> = Arc::new(RwLock::new(queue));
-    let mut threads:Vec<std::thread::JoinHandle::<()>> = Vec::with_capacity(THREAD_COUNT);
-    for _ in 0..THREAD_COUNT {
-        let queue_copy = shared_queue.clone();
-        let bar_copy = bar.clone();
-        threads.push(std::thread::spawn(move || worker_loop(queue_copy, bar_copy)));
-    }
-    for t in threads{ let _ = t.join();}
-}
-fn worker_loop(mut queue:QueueType<QueueTask>, mut progress_bar:Option<indicatif::ProgressBar>){
-    while let Some(task) = fetch_task(&mut queue){
-        let new_tasks = exec_task(task);
-        if let Some((t1,t2)) = new_tasks {
-            append_tasks(&mut queue, (t1,t2));
-        }
-        if let Some(bar) = &mut progress_bar{
-            bar.inc(1);
-        }
-    }
-}
-fn fetch_task(queue:&mut QueueType<QueueTask>)->Option<QueueTask>{
-    let handle = queue.write();
-    match handle{
-        Ok(mut handle)=>{
-            handle.pop_front()
-        }
-        Err(_)=>{
-            eprintln!("Error when getting queue handle in thread {:?}", std::thread::current().id());
-            None
-        }
-    }
-}
-fn exec_task(task:QueueTask)->Option<(QueueTask, QueueTask)>{
-    let QueueTask{frac1,frac2,buffer} = task;
-    
-    let middle = frac1+frac2;
-    buffer[buffer.len()/2] = middle;
-    
-    // we don't wanna infinitely add tasks to the queue
-    if buffer.len() == 1 {
-        return None;
-    }
 
-    let (first_half,second_half) = buffer.split_at_mut(buffer.len()/2);
-    Some(
-        (QueueTask{
-            frac1,
-            frac2:middle,
-            buffer:first_half
-        },
-        QueueTask{
-            frac1:middle,
-            frac2,
-            buffer:second_half
-        }))
-}
-fn append_tasks(queue:&mut QueueType<QueueTask>, (task1, task2):(QueueTask,QueueTask)){
-    let mut handle = queue.write().expect("Queue wouldn't accept another task, fractions were lost");
-    handle.push_back(task1);
-    handle.push_back(task2);
-}
-/// Returns all the fractions generated but not f1 and f2 (slow version that uses vec rather than a
-/// buffer)
-fn recurse(f1:Frac,f2:Frac, remaining:u64, progress:Option<indicatif::ProgressBar>)->Vec<Frac>{
-    if remaining == 0 {
-        return Vec::new()
-    }
-    let middle = f1+f2;
-    if let Some(bar) = progress.clone(){
-        bar.inc(1);
-    }
-    let left = recurse(f1,middle,remaining-1,progress.clone());
-    let mut right = recurse(middle,f2,remaining-1,progress);
-    
-    debug_assert!(left.len() == 0 || left[left.len()-1]<middle,"left assertion failed {} not < {}", left[left.len()-1],middle);
-    debug_assert!(right.len() == 0 || right[0]<middle,"right assertion failed {} not > {}", right[right.len()-1],middle);
+fn recurse(
+    f1: Frac,
+    f2: Frac,
+    max_depth: u32,
+    progress: Option<indicatif::ProgressBar>,
+) -> Vec<Frac> {
+    let ret_size = 2_usize.pow(max_depth) + 1;
+    let mut stack: VecDeque<Task> = VecDeque::new();
+    stack.push_back(Task {
+        f1: (f1, 0),
+        f2: (f2, ret_size - 1),
+        remaining_depth: max_depth,
+    });
 
-    let mut ret = left;
-    ret.reserve(ret.len()+right.len()+1);
-    ret.push(middle);
-    ret.append(&mut right);
+    let mut ret: Vec<MaybeUninit<Frac>> =
+        Vec::from_iter((0..ret_size).map(|_| MaybeUninit::uninit()));
+    ret[0] = MaybeUninit::new(f1);
+    ret[ret_size - 1] = MaybeUninit::new(f2);
+    while let Some(task) = stack.pop_back() {
+        if task.remaining_depth == 0 {
+            continue;
+        }
+        let midpoint = (task.f1.1 + task.f2.1) / 2;
+        //eprintln!("{} {} {}", task.f1.1, midpoint, task.f2.1);
+        let midpoint_value = task.f1.0 + task.f2.0;
+        ret[midpoint] = MaybeUninit::new(midpoint_value);
+        if let Some(progress) = progress.clone() {
+            progress.inc(1);
+        }
+        stack.push_back(Task {
+            f1: task.f1,
+            f2: (midpoint_value, midpoint),
+            remaining_depth: task.remaining_depth - 1,
+        });
+        stack.push_back(Task {
+            f1: (midpoint_value, midpoint),
+            f2: task.f2,
+            remaining_depth: task.remaining_depth - 1,
+        });
+    }
 
     return ret
+        .into_iter()
+        .map(|frac| {
+            // Safety: if we've generated every fraction down to max_depth then every slot in the Vec
+            // should be filled
+            unsafe { frac.assume_init() }
+        })
+        .collect();
 }
 
-#[derive(Clone, Copy, Debug,PartialEq)]
-struct Frac(u32,u32);
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Frac(u32, u32);
 
-impl PartialOrd for Frac{
+impl PartialOrd for Frac {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         f64::from(*self).partial_cmp(&f64::from(*other))
     }
 }
-impl From<Frac> for f64{
+impl From<Frac> for f64 {
     fn from(value: Frac) -> Self {
-        (value.0 as f64)/(value.1 as f64)
+        (value.0 as f64) / (value.1 as f64)
     }
 }
-impl Add for Frac{
+impl Add for Frac {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        Frac(self.0+rhs.0,self.1+rhs.1)
+        Frac(self.0 + rhs.0, self.1 + rhs.1)
     }
 }
-impl Display for Frac{
+impl Display for Frac {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{}/{}",self.0,self.1))
+        f.write_fmt(format_args!("{}/{}", self.0, self.1))
     }
 }
